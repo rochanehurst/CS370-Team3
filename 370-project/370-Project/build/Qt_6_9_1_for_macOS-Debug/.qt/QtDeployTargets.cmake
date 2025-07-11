3ca8d4d593277b7bf559d504d9cf15475ff93363
Roxie H <78993966+rochanehurst@users.noreply.github.com>
2025-07-11 15:15:17 -0700

    Adding 370-project Qt base files

---
+++ 370-project/370-Project/build/Qt_6_9_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_370-Project_FILE /Users/rochanehurst/Documents/CSUSM/370-project/370-Project/build/Qt_6_9_1_for_macOS-Debug/370-Project.app/Contents/MacOS/370-Project)
+set(__QT_DEPLOY_TARGET_370-Project_TYPE EXECUTABLE)

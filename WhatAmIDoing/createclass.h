f77fcb499b2294d28850263bb3f5e25b688b8965
Ruby Leahy <colee.leahy@gmail.com>
2025-07-14 18:08:01 -0700

    Cloned Cassidy's Branch. Changed items. Description Explains
    Changed class adder to be a form.
    Removed length, replaced with online boolean.
    Added Class Creation Restrictions
    Classes will now be saved as a struct when user clicks "OK"

--- WhatAmIDoing/createclass.h
+++ WhatAmIDoing/createclass.h
@@ -3,6 +3,30 @@
 
 #include <QDialog>
 #include <QStringListModel>
+#include <QCheckBox>
+
+using namespace std;
+
+struct ClassInfo {
+    QString school;
+    QString name;
+    QString building;
+    QString startTime;
+    QString endTime;
+    QString days;
+    bool online;
+
+    void print() const{
+        qDebug() << "School: " << school
+                 << "\nName: " << name
+                 << "\nBuilding: " << building
+                 << "\nStarts at: " << startTime
+                 << "\nEnds at:" << endTime
+                 << "\nOn days: " << days
+                 << "\nIs Online?: " << online;
+    }
+};
+
 namespace Ui {
 class Dialog;
 }
@@ -16,21 +40,30 @@ public:
     ~Dialog();
 
 private slots:
-    void on_addDay_clicked();
 
-    void on_removeDay_clicked();
+    void on_confirmButtonBox_accepted();
 
-    void update();
+    void on_confirmButtonBox_rejected();
 
-    void on_buttonBox_accepted();
+    void fourDayChecker(QObject *sender, bool disable);
 
-    void on_buttonBox_rejected();
+    void on_MWF_stateChanged(int arg1);
+
+    void on_TR_stateChanged(int arg1);
+
+    void createClass();
+
+    QString dayStringCreate();
+
+    void on_timeStart_userTimeChanged(const QTime &time);
+
+    void on_timeStop_userTimeChanged(const QTime &time);
 
 private:
     Ui::Dialog *ui;
     QStringListModel *model;
-    QStringList days = {};
-    const QStringList dayOrder = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    QVector<QCheckBox*> checkboxes;
+    vector<ClassInfo*> classes;
 };
 
 #endif // CREATECLASS_H

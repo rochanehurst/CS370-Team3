801a132481cfa2a6ddbd139c29d085ed8efd96e1
Ruby Leahy <colee.leahy@gmail.com>
2025-07-15 08:51:56 -0700

    cmake update

---
+++ 370-project/370-Project/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_370-Project_FILE C:/Users/coleo/Desktop/CS370-Team3-Github/370-project/370-Project/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/370-Project.exe)
+set(__QT_DEPLOY_TARGET_370-Project_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_370-Project_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)

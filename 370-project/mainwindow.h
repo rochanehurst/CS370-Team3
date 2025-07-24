b9c115536476816c6268d2c880c08cba8c5d3292
klein057 <klein057@csusm.edu>
2025-07-23 19:33:12 -0700

    Update mainwindow.h

--- 370-project/mainwindow.h
+++ 370-project/mainwindow.h
@@ -23,6 +23,11 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+enum class IsOnline{
+    Yes,
+    No
+};
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -34,21 +39,37 @@ public:
 
 private slots:
     void createClassButtonHandler();
-
     void createClassFrame(const ClassInfo& class_info);
 
-    void deleteClassFrame(ClassInfoFrame* class_data);
+    void editSave();
+    void removeFromSave();
+    void clearSchedule()
 
-    void editClassFrame(ClassInfoFrame* class_data);
+    void debugPopulateList();
+
+    //void deleteClassFrame(ClassInfoFrame* class_data);
+
+    //void editClassFrame(ClassInfoFrame* class_data);
+
+private:
+    void setupConnections();
+    void setupClassListLayout();
+
+    void debugAddClassToList(ClassInfo* tester);
+    ClassInfo* debugCreateClass(QString name,
+                          QString days,
+                          QString start,
+                          QString end,
+                          IsOnline online,
+                          QString building = "Online Class");
 
 private:
     Ui::MainWindow *ui;
-
     QVector<ClassInfo> class_info;  // stores the data model
 
-    QVector<ClassInfoFrame*> class_data_containers;  // stores the actual QFrame widgets
+    //QVector<ClassInfoFrame*> class_data_containers;  // stores the actual QFrame widgets
 
-    QVBoxLayout* classListLayout;  // layout where the frames get added
+    QVBoxLayout* class_List_Layout;  // layout where the frames get added
 };
 
 #endif // MAINWINDOW_H

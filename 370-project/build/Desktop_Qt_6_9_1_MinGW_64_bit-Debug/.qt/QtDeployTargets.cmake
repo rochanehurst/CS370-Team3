be328fbb67564c571cb28af8790e0010d02d9218
Cole Leahy <colee.leahy@gmail.com>
2025-07-30 22:38:17 -0700

    Deleting User Files

--- 370-project/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_370-Project_FILE C:/crying/CS370-Team3/370-project/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/370-Project.exe)
-set(__QT_DEPLOY_TARGET_370-Project_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_370-Project_RUNTIME_DLLS C:/QT/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/QT/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/QT/6.9.1/mingw_64/bin/Qt6Core.dll)

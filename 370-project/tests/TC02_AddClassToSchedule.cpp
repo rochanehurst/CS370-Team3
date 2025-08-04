9c24de4c39b031364889f01f54dd39f66d3be134
Roxie H <78993966+rochanehurst@users.noreply.github.com>
2025-08-04 10:28:57 -0700

    Update unit tests TC01–TC10, schedule logic, and CMake config

--- 370-project/tests/TC02_AddClassToSchedule.cpp
+++ 370-project/tests/TC02_AddClassToSchedule.cpp
@@ -6,30 +6,17 @@ class TC02_AddClassToSchedule : public QObject {
     Q_OBJECT
 
 private slots:
-    void test_addOneClass() {
-        // Create empty schedule
+    void test_addClassToVector() {
+        // Test adding a ClassInfo to a QVector (unit test for data structure)
         QVector<ClassInfo> schedule;
 
-        // Create a simple test class
         ClassInfo testClass;
         testClass.name = "CS111";
-        testClass.building = "MARK 204";
-        testClass.startTime = "10:00AM";
-        testClass.endTime = "11:00AM";
-        testClass.days = "MW";
 
-        // Add to schedule
         schedule.append(testClass);
 
-        // Check it was added
         QCOMPARE(schedule.size(), 1);
         QCOMPARE(schedule[0].name, QString("CS111"));
-    }
-
-    void test_scheduleStartsEmpty() {
-        // New schedule should be empty
-        QVector<ClassInfo> schedule;
-        QVERIFY(schedule.isEmpty());
     }
 };
 
--- 370-project/tests/TC03_ScheduleConflictDetection.cpp
+++ 370-project/tests/TC03_ScheduleConflictDetection.cpp
@@ -6,45 +6,21 @@ class TC03_ScheduleConflictDetection : public QObject {
     Q_OBJECT
 
 private slots:
-    void test_noConflictDifferentDays() {
+    void test_conflictDetection() {
+        // Unit test for classesConflict() function
         ScheduleLogic logic;
 
-        // Monday class
-        ClassInfo mondayClass;
-        mondayClass.name = "Math";
-        mondayClass.days = "M";
-        mondayClass.startTime = "10:00AM";
-        mondayClass.endTime = "11:00AM";
-
-        // Tuesday class (different day)
-        ClassInfo tuesdayClass;
-        tuesdayClass.name = "English";
-        tuesdayClass.days = "T";
-        tuesdayClass.startTime = "10:00AM";  // Same time is OK
-        tuesdayClass.endTime = "11:00AM";
-
-        // Should NOT conflict (different days)
-        QVERIFY(!logic.classesConflict(mondayClass, tuesdayClass));
-    }
-
-    void test_conflictSameTime() {
-        ScheduleLogic logic;
-
-        // Monday 10am class
         ClassInfo class1;
-        class1.name = "Math";
         class1.days = "M";
         class1.startTime = "10:00AM";
         class1.endTime = "11:00AM";
 
-        // Another Monday 10am class
         ClassInfo class2;
-        class2.name = "Science";
-        class2.days = "M";  // Same day!
-        class2.startTime = "10:00AM";  // Same time!
-        class2.endTime = "11:00AM";
+        class2.days = "M";
+        class2.startTime = "10:30AM";
+        class2.endTime = "11:30AM";
 
-        // Should conflict
+        // Test the conflict detection function
         QVERIFY(logic.classesConflict(class1, class2));
     }
 };
